17e450fc2aa61f3920ad893420b2a723f2cb227e
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-05-16 09:30:44 +0800

    Stage 136: force Android soft input adjustNothing at runtime

--- android/app/src/main/kotlin/com/example/new_twitch_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/new_twitch_app/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.new_twitch_app
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
+        super.onCreate(savedInstanceState)
+    }
+}
