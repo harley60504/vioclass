d396919a2059503280925c298cb610bb6a07db37
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-05-19 11:01:03 +0800

    Stage 226 wire Android PiP bridge into MainActivity

--- android/app/src/main/kotlin/com/example/new_twitch_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/new_twitch_app/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.new_twitch_app
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
+    private var pipBridge: TwitchPipBridge? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        val bridge = TwitchPipBridge(this)
+        bridge.attach(MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "vio_class/android_pip"))
+        pipBridge = bridge
+    }
+
+    override fun onDestroy() {
+        pipBridge?.detach()
+        pipBridge = null
+        super.onDestroy()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        pipBridge?.notifyPipModeChanged(isInPictureInPictureMode)
     }
 }
