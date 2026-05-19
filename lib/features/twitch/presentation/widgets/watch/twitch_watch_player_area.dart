379427c8755dd9e447a3778e185cb8c4dfa0bb8d
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-05-19 21:27:02 +0800

    Stage 230F replace error card part with import

--- lib/features/twitch/presentation/widgets/watch/twitch_watch_player_area.dart
+++ lib/features/twitch/presentation/widgets/watch/twitch_watch_player_area.dart
@@ -17,6 +17,7 @@ import '../../../services/playback/twitch_playlist_player_runtime.dart';
 import '../shared/twitch_glass.dart';
 import 'player/twitch_media_kit_video_surface.dart';
 import 'player/twitch_player_common_buttons.dart';
+import 'player/twitch_player_error_card.dart';
 import 'player/twitch_player_more_actions_button.dart';
 import 'player/twitch_player_pip_button.dart';
 import 'player/twitch_player_quality_button.dart';
@@ -28,7 +29,6 @@ part 'player/twitch_watch_stream_header.dart';
 part 'player/twitch_watch_top_buttons.dart';
 part 'player/twitch_watch_bottom_control_bar.dart';
 part 'player/twitch_live_playback_strip.dart';
-part 'player/twitch_player_error_card.dart';
 
 class TwitchWatchPlayerArea extends StatelessWidget {
   final TwitchPlaylistPlayerRuntime playerRuntime;
