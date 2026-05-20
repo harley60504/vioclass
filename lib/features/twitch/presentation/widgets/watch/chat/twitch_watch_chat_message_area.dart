0070e64b394ad91d3d7187c0cc300e25278cd051
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-05-20 19:36:48 +0800

    Stage 246 share official emote cache with chat renderer

--- lib/features/twitch/presentation/widgets/watch/chat/twitch_watch_chat_message_area.dart
+++ lib/features/twitch/presentation/widgets/watch/chat/twitch_watch_chat_message_area.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../models/chat/twitch_chat_runtime_message.dart';
 import '../../../../services/chat/twitch_chat_runtime.dart';
+import '../../../../services/chat/twitch_official_emote_cache_service.dart';
 import '../../../../services/chat/twitch_third_party_emote_cache_service.dart';
 import '../../chat/twitch_chat_empty_view.dart';
 import '../../chat/twitch_chat_message_list.dart';
@@ -9,6 +10,7 @@ import '../../chat/twitch_chat_message_list.dart';
 class TwitchWatchChatMessageArea extends StatelessWidget {
   final TwitchChatRuntime? runtime;
   final TwitchThirdPartyEmoteCacheService thirdPartyEmoteCache;
+  final TwitchOfficialEmoteCacheService officialEmoteCache;
   final Listenable appearanceListenable;
   final double fontScale;
   final bool compact;
@@ -18,6 +20,7 @@ class TwitchWatchChatMessageArea extends StatelessWidget {
     super.key,
     required this.runtime,
     required this.thirdPartyEmoteCache,
+    required this.officialEmoteCache,
     required this.appearanceListenable,
     required this.fontScale,
     required this.compact,
@@ -32,12 +35,15 @@ class TwitchWatchChatMessageArea extends StatelessWidget {
     return AnimatedBuilder(
       animation: Listenable.merge([
         currentRuntime,
+        thirdPartyEmoteCache,
+        officialEmoteCache,
         appearanceListenable,
       ]),
       builder: (context, _) {
         return TwitchChatMessageList(
           runtime: currentRuntime,
           thirdPartyEmoteCache: thirdPartyEmoteCache,
+          officialEmoteCache: officialEmoteCache,
           fontScale: fontScale,
           compact: compact,
           onOpenMessageContext: onOpenMessageContext,
