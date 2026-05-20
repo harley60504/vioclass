6c266b808f3a72ed3211cf866a75ae27caf15639
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-05-20 19:39:47 +0800

    Stage 246 pass official emote cache through runtime message tile

--- lib/features/twitch/presentation/widgets/chat/twitch_runtime_message_tile.dart
+++ lib/features/twitch/presentation/widgets/chat/twitch_runtime_message_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../models/chat/twitch_chat_runtime_message.dart';
+import '../../../services/chat/twitch_official_emote_cache_service.dart';
 import '../../../services/chat/twitch_third_party_emote_cache_service.dart';
 import 'message/twitch_chat_message_cards.dart';
 import 'message/twitch_chat_message_special_style.dart';
@@ -10,6 +11,7 @@ import 'message/twitch_chat_message_visual_metrics.dart';
 class TwitchRuntimeMessageTile extends StatelessWidget {
   final TwitchChatRuntimeMessage message;
   final TwitchThirdPartyEmoteCacheService? thirdPartyEmotes;
+  final TwitchOfficialEmoteCacheService? officialEmotes;
   final bool showTimestamp;
   final double fontScale;
   final bool compact;
@@ -19,6 +21,7 @@ class TwitchRuntimeMessageTile extends StatelessWidget {
     super.key,
     required this.message,
     this.thirdPartyEmotes,
+    this.officialEmotes,
     this.showTimestamp = false,
     this.fontScale = 1.0,
     this.compact = false,
@@ -39,6 +42,7 @@ class TwitchRuntimeMessageTile extends StatelessWidget {
       return TwitchChatSpecialMessageCard(
         message: message,
         thirdPartyEmotes: thirdPartyEmotes,
+        officialEmotes: officialEmotes,
         displayColor: displayColor,
         displayNameText: displayNameText,
         style: style,
@@ -51,6 +55,7 @@ class TwitchRuntimeMessageTile extends StatelessWidget {
     return TwitchChatNormalMessageCard(
       message: message,
       thirdPartyEmotes: thirdPartyEmotes,
+      officialEmotes: officialEmotes,
       displayColor: displayColor,
       displayNameText: displayNameText,
       showTimestamp: showTimestamp,
