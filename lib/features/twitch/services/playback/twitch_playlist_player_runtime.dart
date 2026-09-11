ff38225c5ab8ad0dca9002f1a5ee8b4d713b1ef5
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-09-12 02:56:42 +0800

    Restore playback candidate field name

--- lib/features/twitch/services/playback/twitch_playlist_player_runtime.dart
+++ lib/features/twitch/services/playback/twitch_playlist_player_runtime.dart
@@ -1205,14 +1205,14 @@ class TwitchPlaylistPlayerRuntime extends ChangeNotifier {
 
 class _PlaybackCandidate {
   final String sourceTag;
-  final Uri masterPlaylistUri;
+  final Uri masterUri;
   final String masterPlaylistText;
   final TwitchPlaybackAccessToken token;
   final List<TwitchM3u8Variant> variants;
 
   const _PlaybackCandidate({
     required this.sourceTag,
-    required this.masterPlaylistUri,
+    required this.masterUri,
     required this.masterPlaylistText,
     required this.token,
     required this.variants,
