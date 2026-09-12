b1543e3783f57e58c3a342e5deda68f730442212
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-09-13 01:46:41 +0800

    refactor(watch): split live and recorded watch modes

--- lib/features/twitch/presentation/watch/twitch_watch_playback_kind.dart
+++ lib/features/twitch/presentation/watch/twitch_watch_playback_kind.dart
@@ -1,1 +1,17 @@
 enum TwitchWatchPlaybackKind { none, live, liveDvr, vod, clip }
+
+enum TwitchWatchMode { liveWatch, recordedWatch }
+
+extension TwitchWatchPlaybackKindMode on TwitchWatchPlaybackKind {
+  TwitchWatchMode get watchMode {
+    switch (this) {
+      case TwitchWatchPlaybackKind.live:
+      case TwitchWatchPlaybackKind.liveDvr:
+        return TwitchWatchMode.liveWatch;
+      case TwitchWatchPlaybackKind.vod:
+      case TwitchWatchPlaybackKind.clip:
+      case TwitchWatchPlaybackKind.none:
+        return TwitchWatchMode.recordedWatch;
+    }
+  }
+}
