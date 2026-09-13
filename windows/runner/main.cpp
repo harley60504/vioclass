b82b94ae4d864156fb13c7a7a8b10d02ef27bce2
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-09-13 16:52:36 +0800

    revert: remove CEF Windows runner hooks

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -4,17 +4,9 @@
 
 #include "flutter_window.h"
 #include "utils.h"
-#include "webview_cef/webview_cef_plugin_c_api.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  // CEF uses Chromium's multi-process architecture. Sub-processes must exit
-  // before Flutter/COM initialization in the host process.
-  int cef_exit_code = initCEFProcesses(instance);
-  if (cef_exit_code >= 0) {
-    return cef_exit_code;
-  }
-
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -44,7 +36,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
-    handleWndProcForCEF(msg.hwnd, msg.message, msg.wParam, msg.lParam);
   }
 
   ::CoUninitialize();
